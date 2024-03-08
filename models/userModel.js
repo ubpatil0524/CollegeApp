23b675e502f27d6ee493fcea2a32101d71a11f0d
mohinivibhute <mohinivbaisolutions@gmail.com>
2024-03-08 18:35:04 +0530

    register and login changes on schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,31 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({ 
-  username: {
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  phoneNo: {
+    type: Number,
+    required: true,
+  },
+  email: {
     type: String,
     required: true,
   },
   password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
     type: String,
     required: true,
   },
